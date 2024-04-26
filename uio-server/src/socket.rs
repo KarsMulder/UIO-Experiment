b719ac9a1f3637b6ba9892a8d9d2c9c246a47ba5
Kars Mulder <devmail@karsmulder.nl>
2024-04-26 15:33:13 +0200

    Implement SeqPacketChannel with rustix.

--- uio-server/src/socket.rs
+++ uio-server/src/socket.rs
@@ -1,17 +1,26 @@
+use std::io::IoSliceMut;
 use std::mem::MaybeUninit;
 use std::os::fd::{AsRawFd, OwnedFd};
 use std::path::PathBuf;
 
+use rustix::fs::OFlags;
+use rustix::io::FdFlags;
+use rustix::net::{RecvAncillaryBuffer, RecvAncillaryMessage, RecvFlags};
+
 use crate::fs_utils::UnlinkOnDrop;
 
-struct Packet {
-    data: Vec<u8>,
+/// A message that can be send through a SeqPacketChannel. It is a vector of bytes that optionally contains
+/// space for file descriptors.
+pub struct Packet {
+    pub data: Vec<u8>,
+    pub fds: Vec<OwnedFd>,
 }
 
 impl Packet {
     pub fn empty() -> Self {
         Packet {
             data: Vec::new(),
+            fds: Vec::new(),
         }
     }
 }
@@ -33,8 +42,8 @@ impl SeqPacketSocket {
         let socket = rustix::net::socket(rustix::net::AddressFamily::UNIX, rustix::net::SocketType::SEQPACKET, None)?;
 
         // Give the file descriptor the proper flags.
-        rustix::fs::fcntl_setfd(&socket, rustix::fs::FdFlags::CLOEXEC)?;
-        rustix::fs::fcntl_setfl(&socket, rustix::fs::OFlags::NONBLOCK)?;
+        rustix::fs::fcntl_setfd(&socket, FdFlags::CLOEXEC)?;
+        rustix::fs::fcntl_setfl(&socket, OFlags::NONBLOCK)?;
 
         // Bind the socket to the filesystem.
         let socket_name = rustix::net::SocketAddrUnix::new(path.clone())?;
@@ -64,55 +73,52 @@ impl std::os::fd::AsFd for SeqPacketSocket {
 
 impl SeqPacketChannel {
     pub fn read_packet(&mut self) -> Result<Packet, std::io::Error> {
-        const MSG_BUF_SIZE: usize = 4096;
-        const MSG_CONTROL_BUF_SIZE: usize = 1024;
+        const MSG_BUF_SIZE: usize = 16 * 1024;
 
-        let mut msg_buf: MaybeUninit<[u8; MSG_BUF_SIZE]> = MaybeUninit::uninit();
-        let mut msg_control: MaybeUninit<[u8; MSG_CONTROL_BUF_SIZE]> = MaybeUninit::uninit();
+        loop {
+            // ... I'm not a fan of how rustix requires us to zero-init the whole buffer, but then again, I have
+            // better things to do right now than micro-optimizations.
+            let mut msg_buf: [u8; MSG_BUF_SIZE] = [0; MSG_BUF_SIZE];
+            let mut ioslice = [IoSliceMut::new(&mut msg_buf)];
 
-        let mut msg_iov: libc::iovec = libc::iovec {
-            iov_base: msg_buf.as_mut_ptr() as *mut libc::c_void,
-            iov_len: std::mem::size_of_val(&msg_buf),
-        };
+            let mut control_space = [0; rustix::cmsg_space!(ScmRights(64))];
+            let mut control_buf = RecvAncillaryBuffer::new(&mut control_space);
 
-        let mut msghdr = libc::msghdr {
-            msg_name: std::ptr::null_mut(),
-            msg_namelen: 0,
-            msg_iov: &mut msg_iov as *mut libc::iovec,
-            msg_iovlen: std::mem::size_of_val(&msg_iov),
-            msg_control: &mut msg_control as *mut _ as *mut libc::c_void,
-            msg_controllen: std::mem::size_of_val(&msg_control),
-            msg_flags: 0
-        };
+            let res = rustix::net::recvmsg(&self, &mut ioslice, &mut control_buf, RecvFlags::CMSG_CLOEXEC)?;
 
-        let res = unsafe {
-            libc::recvmsg(self.fd.as_raw_fd(), &mut msghdr, libc::MSG_CMSG_CLOEXEC)
-        };
-        if res < 0 {
-            return Err(std::io::Error::last_os_error());
+            // TODO: This can cause out-of-memory when dealing with a malicious client.
+            let message = &msg_buf[0 .. res.bytes];
+            self.read_buffer.data.extend_from_slice(message);
+
+            // TODO: In production code, all of the following instances of panic! are obviously unacceptable.
+            if res.flags.contains(RecvFlags::TRUNC) {
+                panic!("Part of a message was truncated!");
+            }
+            if res.flags.contains(RecvFlags::ERRQUEUE) {
+                panic!("Received error message through socket!");
+            }
+            // For some reason rustix doesn't contain this flag. Maybe I should send a pull request?
+            if res.flags.bits() & (libc::MSG_CTRUNC as u32) > 0 {
+                panic!("Part of control data was discarded!");
+            }
+
+            for control_msg in control_buf.drain() {
+                match control_msg {
+                    RecvAncillaryMessage::ScmRights(fds) => self.read_buffer.fds.extend(fds),
+                    RecvAncillaryMessage::ScmCredentials(_) => panic!("Received credentials!"),
+                    _ => panic!("Received unknown ancillary data!"),
+                }
+            }
+
+            // Check there is an end-of-packed delimination at the end of this message.
+            if res.flags.bits() & (libc::MSG_EOR as u32) > 0 {
+                let empty_buffer = Packet::empty();
+                return Ok(std::mem::replace(&mut self.read_buffer, empty_buffer));
+            } else {
+                // Keep trying again until we either get a complete message or fail by EWOULDBLOCK or EAGAIN.
+                println!("Message exceeded buffer size, reading again...");
+            }
         }
-        if res == 0 {
-            return Err(std::io::Error::new(std::io::ErrorKind::ConnectionReset, "The peer has performed an orderly shutdown."));
-        }
-
-        // TODO: In production code, this obviously shouln't panic.
-        if msghdr.msg_flags & libc::MSG_TRUNC > 0 {
-            panic!("Part of a message was truncated!");
-        }
-        if msghdr.msg_flags & libc::MSG_CTRUNC > 0 {
-            panic!("Part of control data was discarded!");
-        }
-        if msghdr.msg_flags & libc::MSG_ERRQUEUE > 0 {
-            panic!("Received error message through socket!");
-        }
-
-        // Check there is an end-of-packed delimination at the end of this message.
-        if msghdr.msg_flags & libc::MSG_EOR > 0 {
-
-        }
-
-        unimplemented!()
-
     }
 }
 
